86c18baab70b59ae8bfe20dec30fdfc0238b7bec
pramu <pramudithagn@gmail.com>
2021-10-24 10:05:00 +0530

    added forum & comment text field

---
+++ app/src/main/java/com/example/myapplication4/Model/Comments.java
@@ -0,0 +1,14 @@
+package com.example.myapplication4.Model;
+
+public class Comments extends CommentsId{
+    private String comment , user;
+
+    public String getComment() {
+        return comment;
+    }
+
+    public String getUser() {
+        return user;
+    }
+}
+
